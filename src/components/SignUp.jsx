097554a772e38f711f368630272572b18deb9882
wtlin1228 <wtlin1228@gmail.com>
2020-04-22 22:07:27 +0800

    add user signup with username/password method

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,18 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { auth } from "../firebase";
 
 class SignUp extends Component {
-  state = { displayName: '', email: '', password: '' };
+  state = { displayName: "", email: "", password: "" };
 
-  handleChange = event => {
+  handleChange = (event) => {
     const { name, value } = event.target;
 
     this.setState({ [name]: value });
   };
 
-  handleSubmit = event => {
+  handleSubmit = async (event) => {
     event.preventDefault();
 
-    this.setState({ displayName: '', email: '', password: '' });
+    const { email, password, displayName } = this.state;
+    try {
+      const { user } = await auth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+
+      user.updateProfile({ displayName });
+    } catch (error) {
+      console.error(error);
+    }
+
+    this.setState({ displayName: "", email: "", password: "" });
   };
 
   render() {
